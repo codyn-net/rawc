2b19e0b683504bd07518683ade90e913e8a1cb6b
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2011-04-28 18:17:19 +0200

    Only use parentheses when needed

--- libcpgrawc/Programmer/Formatters/C/InstructionTranslator.cs
+++ libcpgrawc/Programmer/Formatters/C/InstructionTranslator.cs
@@ -1,13 +1,59 @@
 using System;
+using System.Collections.Generic;
 
 namespace Cpg.RawC.Programmer.Formatters.C
 {
 	public class InstructionTranslator : DynamicVisitor
 	{
+		class OperatorSpec
+		{
+			public Cpg.MathOperatorType Type;
+			public int Priority;
+			public bool LeftAssociation;
+			
+			public OperatorSpec(Cpg.MathOperatorType type, int priority, bool leftAssociation)
+			{
+				Type = type;
+				Priority = priority;
+				LeftAssociation = leftAssociation;
+			}
+		}
+
+		private static Dictionary<Cpg.MathOperatorType, OperatorSpec> s_operatorSpecs;
+		
+		private static void AddSpec(Cpg.MathOperatorType type, int priority, bool leftAssociation)
+		{
+			s_operatorSpecs[type] = new OperatorSpec(type, priority, leftAssociation);
+		}
+		
+		static InstructionTranslator()
+		{
+			s_operatorSpecs = new Dictionary<MathOperatorType, OperatorSpec>();
+			
+			AddSpec(MathOperatorType.Multiply, 7, true);
+			AddSpec(MathOperatorType.Divide, 7, true);
+			AddSpec(MathOperatorType.Modulo, 7, true);
+			AddSpec(MathOperatorType.Plus, 6, true);
+			AddSpec(MathOperatorType.Minus, 6, true);
+			AddSpec(MathOperatorType.UnaryMinus, 8, false);
+			
+			AddSpec(MathOperatorType.Negate, 8, false);
+			AddSpec(MathOperatorType.Greater, 5, true);
+			AddSpec(MathOperatorType.Less, 5, true);
+			AddSpec(MathOperatorType.GreaterOrEqual, 5, true);
+			AddSpec(MathOperatorType.LessOrEqual, 5, true);
+			AddSpec(MathOperatorType.Equal, 4, true);
+			AddSpec(MathOperatorType.Or, 2, true);
+			AddSpec(MathOperatorType.And, 3, true);
+			
+			AddSpec(MathOperatorType.Ternary, 1, false);
+		}
+
 		public InstructionTranslator() : base(typeof(string),
 		                                      BindingFlags.Default,
 		                                      System.Reflection.BindingFlags.Default |
 		                                      System.Reflection.BindingFlags.NonPublic |
+		                                      System.Reflection.BindingFlags.Public |
 		                                      System.Reflection.BindingFlags.Instance |
 		                                      System.Reflection.BindingFlags.InvokeMethod,
 		                                      new Type[] {typeof(Instruction), typeof(Context)})
@@ -42,9 +88,28 @@ namespace Cpg.RawC.Programmer.Formatters.C
 			return ret;
 		}
 
-		private string SimpleOperator(Context context, string glue)
+		private bool HasPriority(Cpg.MathOperatorType a, Cpg.MathOperatorType b)
+		{
+			OperatorSpec s1;
+			OperatorSpec s2;
+			
+			if (!s_operatorSpecs.TryGetValue(a, out s1) || !s_operatorSpecs.TryGetValue(b, out s2))
+			{
+				return false;
+			}
+			
+			return s1.Priority > s2.Priority || (s1.Priority == s2.Priority && !s2.LeftAssociation);
+		}
+
+		private string SimpleOperator(Context context, InstructionOperator inst, string glue)
 		{
 			int num = context.Node.Children.Count;
+			
+			if (num == 1)
+			{
+				return String.Format("{0}{1}", glue, Translate(context, 0)).Trim();
+			}
+
 			string[] args = new string[num];
 			
 			for (int i = 0; i < num; ++i)
@@ -52,7 +117,23 @@ namespace Cpg.RawC.Programmer.Formatters.C
 				args[i] = Translate(context, i);
 			}
 			
-			return String.Format("({0})", String.Join(glue, args).Trim());
+			bool needsparen = false;
+			
+			if (context.Node.Parent != null)
+			{
+				InstructionOperator op = context.Node.Parent.Instruction as InstructionOperator;
+				
+				needsparen = inst == null || (op != null && HasPriority((Cpg.MathOperatorType)op.Id, (Cpg.MathOperatorType)inst.Id));
+			}
+			
+			if (!needsparen)
+			{
+				return String.Join(glue, args).Trim();
+			}
+			else
+			{
+				return String.Format("({0})", String.Join(glue, args).Trim());
+			}
 		}
 		
 		private string Translate(InstructionNumber instruction, Context context)
@@ -65,35 +146,35 @@ namespace Cpg.RawC.Programmer.Formatters.C
 			switch ((Cpg.MathOperatorType)instruction.Id)
 			{
 				case MathOperatorType.And:
-					return SimpleOperator(context, " && ");
+					return SimpleOperator(context, instruction, " && ");
 				case MathOperatorType.Divide:
-					return SimpleOperator(context, " / ");
+					return SimpleOperator(context, instruction, " / ");
 				case MathOperatorType.Equal:
-					return SimpleOperator(context, " == ");
+					return SimpleOperator(context, instruction, " == ");
 				case MathOperatorType.Greater:
-					return SimpleOperator(context, " > ");
+					return SimpleOperator(context, instruction, " > ");
 				case MathOperatorType.GreaterOrEqual:
-					return SimpleOperator(context, " >= ");
+					return SimpleOperator(context, instruction, " >= ");
 				case MathOperatorType.Less:
-					return SimpleOperator(context, " < ");
+					return SimpleOperator(context, instruction, " < ");
 				case MathOperatorType.LessOrEqual:
-					return SimpleOperator(context, " <= ");
+					return SimpleOperator(context, instruction, " <= ");
 				case MathOperatorType.Minus:
-					return SimpleOperator(context, " -");
+					return SimpleOperator(context, instruction, " - ");
 				case MathOperatorType.UnaryMinus:
-					return SimpleOperator(context, " -");
+					return SimpleOperator(context, instruction, " -");
 				case MathOperatorType.Multiply:
-					return SimpleOperator(context, " * ");
+					return SimpleOperator(context, instruction, " * ");
 				case MathOperatorType.Negate:
-					return SimpleOperator(context, " !");
+					return SimpleOperator(context, instruction, " !");
 				case MathOperatorType.Or:
-					return SimpleOperator(context, " || ");
+					return SimpleOperator(context, instruction, " || ");
 				case MathOperatorType.Plus:
-					return SimpleOperator(context, " + ");
+					return SimpleOperator(context, instruction, " + ");
 				case MathOperatorType.Power:
 					return String.Format("{0}{1}",
 					                     Context.MathFunctionDefine(Cpg.MathFunctionType.Pow, context.Node.Children.Count),
-					                     SimpleOperator(context, ", "));
+					                     SimpleOperator(context, null, ", "));
 				case MathOperatorType.Ternary:
 					return String.Format("({0} ? {1} : {2})",
 					                     Translate(context, 0),
